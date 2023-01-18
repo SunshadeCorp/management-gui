274b401d604cff3075f4f14076224bb9e9507d7d
Jesse Kleemann <j.kleemann@tu-bs.de>
2023-01-18 15:36:24 +0100

    total_system_voltage + current + temps + cell colors

--- mqtt_live.py
+++ mqtt_live.py
@@ -45,6 +45,9 @@ class MqttLiveWindow(Ui_MainWindow):
         self.modules: dict = {}
         self.spacer: dict = {}
 
+        self.total_system_voltage: float = 0
+        self.total_system_current: float = 0
+
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.on_connect = self.mqtt_on_connect
         self.mqtt_client.on_message = self.mqtt_on_message
@@ -54,6 +57,7 @@ class MqttLiveWindow(Ui_MainWindow):
         self.mqtt_client.loop_start()
 
         QtCore.QTimer.singleShot(100, self.resize_window)
+        # QtCore.QTimer.singleShot(1000, self.sort_modules)
 
     def show(self):
         self.main_window.show()
@@ -71,6 +75,22 @@ class MqttLiveWindow(Ui_MainWindow):
     def module_dragged(self, infos: dict):
         exchange_widget_positions(self.gridLayout, infos['self'], infos['widget'])
 
+    def sort_modules(self):
+        for i in enumerate(list(self.modules.keys())):
+            print(i)
+        for identifier1 in self.modules:
+            for identifier2 in reversed(self.modules):
+                if identifier2 > identifier1:
+                    exchange_widget_positions(self.gridLayout,
+                                              self.modules[identifier1]['widget'],
+                                              self.modules[identifier2]['widget'])
+        for identifier1 in self.modules:
+            for identifier2 in reversed(self.modules):
+                if identifier2 > identifier1:
+                    exchange_widget_positions(self.gridLayout,
+                                              self.modules[identifier1]['widget'],
+                                              self.modules[identifier2]['widget'])
+
     @staticmethod
     def mqtt_on_connect(client: mqtt.Client, userdata: any, flags: dict, rc: int):
         client.subscribe('esp-module/#')
@@ -87,7 +107,16 @@ class MqttLiveWindow(Ui_MainWindow):
             label = QtWidgets.QLabel(module)
             label.setObjectName("label")
             label.setText(identifier)
+            font = label.font()
+            font.setBold(True)
+            label.setFont(font)
             vertical_layout.addWidget(label)
+            module_temps = QtWidgets.QLabel(module)
+            module_temps.setText('-,-')
+            vertical_layout.addWidget(module_temps)
+            chip_temp = QtWidgets.QLabel(module)
+            chip_temp.setText('-')
+            vertical_layout.addWidget(chip_temp)
             cells: dict = {}
             for i in range(1, 13):
                 cell = QtWidgets.QLabel(module)
@@ -116,6 +145,8 @@ class MqttLiveWindow(Ui_MainWindow):
                 'layout': vertical_layout,
                 'header': label,
                 'cells': cells,
+                'module_temps': module_temps,
+                'chip_temp': chip_temp
             }
 
     def set_widget(self, data: dict):
@@ -127,6 +158,20 @@ class MqttLiveWindow(Ui_MainWindow):
             if data['available'] == 'online':
                 widget: QtWidgets.QWidget = module['widget']
                 widget.setStyleSheet('')
+        elif 'total_system_voltage' in data:
+            self.total_system_voltage = float(data['total_system_voltage'])
+            self.main_window.statusBar().showMessage(f'{self.total_system_voltage} V'
+                                                     f', {self.total_system_current} A'
+                                                     f', {self.total_system_voltage * self.total_system_current:.2f} W')
+        elif 'total_system_current' in data:
+            self.total_system_current = float(data['total_system_current'].split(',')[1]) * -1.0
+            self.main_window.statusBar().showMessage(f'{self.total_system_voltage} V'
+                                                     f', {self.total_system_current} A'
+                                                     f', {self.total_system_voltage * self.total_system_current:.2f} W')
+        elif 'module_temps' in data:
+            module['module_temps'].setText(data['module_temps'])
+        elif 'chip_temp' in data:
+            module['chip_temp'].setText(data['chip_temp'])
         elif 'voltage' in data:
             voltage: float = float(data['voltage'])
             module['cells'][data['number']]['voltage'] = voltage
@@ -143,8 +188,10 @@ class MqttLiveWindow(Ui_MainWindow):
             module['header'].setText(f"{data['identifier']}: {median_voltage:.3f}")
             for cell_number in module['cells']:
                 if 'voltage' in module['cells'][cell_number]:
-                    if abs(module['cells'][cell_number]['voltage'] - median_voltage) >= 0.01:
-                        module['cells'][cell_number]['widget'].setStyleSheet('background-color: red;')
+                    if module['cells'][cell_number]['voltage'] - median_voltage >= 0.01:
+                        module['cells'][cell_number]['widget'].setStyleSheet('background-color: #ff5c33;')
+                    elif module['cells'][cell_number]['voltage'] - median_voltage <= -0.01:
+                        module['cells'][cell_number]['widget'].setStyleSheet('background-color: #3399ff;')
                     else:
                         module['cells'][cell_number]['widget'].setStyleSheet('')
         elif 'is_balancing' in data:
@@ -154,20 +201,68 @@ class MqttLiveWindow(Ui_MainWindow):
             balancing_text = ' (+)' if is_balancing else ''
             cell['widget'].setText(f"{data['number']}: {cell.get('voltage', -1):.3f}{balancing_text}")
 
+    def calc_cell_diff(self):
+        voltages: list[float] = []
+        for ident in self.modules:
+            if len(ident) > 2:
+                continue
+            for cell_number in self.modules[ident]['cells']:
+                cell: dict = self.modules[ident]['cells'][cell_number]
+                if 'voltage' in cell:
+                    voltages.append(cell['voltage'])
+        cell_max: float = max(voltages)
+        cell_min: float = min(voltages)
+        cell_diff: float = (cell_max - cell_min) * 1000
+        cell_median: float = statistics.median(voltages)
+        cell_mean: float = statistics.mean(voltages)
+        self.main_window.setWindowTitle(f'{cell_diff:.0f} mV diff'
+                                        f', {cell_median:.3f} V median'
+                                        f', {cell_mean:.3f} V mean'
+                                        f', {cell_min:.3f} V min'
+                                        f', {cell_max:.3f} V max')
+
     def mqtt_on_message(self, client: mqtt.Client, userdata: any, msg: mqtt.MQTTMessage):
         topic = msg.topic[msg.topic.find('/') + 1:]
         identifier = topic[:topic.find('/')]
+        if len(identifier) > 2:
+            return
         self.main_window.signal.emit({'func': self.add_widget, 'arg': identifier})
         topic = topic[topic.find('/') + 1:]
-        print(identifier, topic)
+        # print(identifier, topic)
         if topic == 'available':
             data = {
                 'identifier': identifier,
                 'available': msg.payload.decode()
             }
             self.main_window.signal.emit({'func': self.set_widget, 'arg': data})
+        elif topic == 'total_system_voltage':
+            data = {
+                'identifier': identifier,
+                'total_system_voltage': msg.payload.decode()
+            }
+            self.main_window.signal.emit({'func': self.set_widget, 'arg': data})
+        elif topic == 'total_system_current':
+            data = {
+                'identifier': identifier,
+                'total_system_current': msg.payload.decode()
+            }
+            self.main_window.signal.emit({'func': self.set_widget, 'arg': data})
+        elif topic == 'chip_temp':
+            data = {
+                'identifier': identifier,
+                'chip_temp': msg.payload.decode()
+            }
+            self.main_window.signal.emit({'func': self.set_widget, 'arg': data})
+        elif topic == 'module_temps':
+            data = {
+                'identifier': identifier,
+                'module_temps': msg.payload.decode()
+            }
+            self.main_window.signal.emit({'func': self.set_widget, 'arg': data})
         elif topic == 'module_topic':
             pass
+        elif topic == 'uptime' and identifier == '1':
+            self.main_window.signal.emit({'func': self.calc_cell_diff})
         elif topic.startswith('cell/'):
             topic = topic[topic.find('/') + 1:]
             number = topic[:topic.find('/')]
