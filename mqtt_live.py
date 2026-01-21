c684b91d1646529d536222ffe6d8b83796edfd0a
Jesse Kleemann <j.kleemann@tu-bs.de>
2026-01-21 01:34:46 +0100

    fix nuitka build

--- mqtt_live.py
+++ mqtt_live.py
@@ -1,3 +1,9 @@
+# nuitka-project: --enable-plugin=pyside6
+# nuitka-project-if: {OS} == "Windows":
+#     nuitka-project: --onefile-cache-mode=cached
+#     nuitka-project: --onefile-tempdir-spec="{PROGRAM_DIR}/.mqtt-live"
+#     nuitka-project: --windows-console-mode=disable
+
 import os
 import statistics
 import sys
